f94dc4fe11a0906bf35a36332656de67567a82c4
Defalt2-O <rithinpaul10@gmail.com>
2024-02-17 16:57:50 +0530

    SingleChildScrollView widget added to allow scrolling of answers.

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -6,28 +6,33 @@ class QuestionsSummary extends StatelessWidget {
   final List<Map<String, Object>> summaryData;
   @override
   Widget build(context) {
-    return Column(
-      children: summaryData.map((data) {
-        return Row(
-          children: [
-            Text(((data['questionIndex'] as int) + 1).toString()),
-            // Type casting is done above as the data eeds to be converted into a string from an integer
-            Expanded(
-              //Expanded forces the content within it to fit within the borders of the immediate parent i.e. Row or Column.
-              child: Column(
-                children: [
-                  Text(data['question'] as String),
-                  const SizedBox(
-                    height: 5,
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map((data) {
+            return Row(
+              children: [
+                Text(((data['questionIndex'] as int) + 1).toString()),
+                // Type casting is done above as the data eeds to be converted into a string from an integer
+                Expanded(
+                  //Expanded forces the content within it to fit within the borders of the immediate parent i.e. Row or Column.
+                  child: Column(
+                    children: [
+                      Text(data['question'] as String),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(data['chosenAnswer'] as String),
+                      Text(data['correctAnswer'] as String),
+                    ],
                   ),
-                  Text(data['chosenAnswer'] as String),
-                  Text(data['correctAnswer'] as String),
-                ],
-              ),
-            )
-          ],
-        );
-      }).toList(),
+                )
+              ],
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
